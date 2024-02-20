d0ff25e0e66b6575161f3fd2ffe39b348d446a16
Bomet1837 <158185256+Bomet1837@users.noreply.github.com>
2024-02-20 21:23:39 +0000

    porting assets and pre-existing work

---
+++ Assets/CameraRig.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform trackingTarget;
+
+    void Update()
+    {
+        transform.position = new Vector3(trackingTarget.position.x, trackingTarget.position.y, transform.position.z);
+    }
+}
