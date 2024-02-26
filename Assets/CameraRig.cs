a57f696358c7214a65447df3f3fa2a5996978e61
Bomet1837 <158185256+Bomet1837@users.noreply.github.com>
2024-02-26 17:54:11 +0000

    26/2/24 additions
    major level changes, additional transition and exit stage

--- Assets/CameraRig.cs
+++ Assets/CameraRig.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CameraRig : MonoBehaviour
 {
+    public float xOffset;
+    public float yOffset;
+    
     [SerializeField]
     protected Transform trackingTarget;
 
     void Update()
     {
-        transform.position = new Vector3(trackingTarget.position.x, trackingTarget.position.y, transform.position.z);
+        transform.position = new Vector3(trackingTarget.position.x + xOffset, trackingTarget.position.y + xOffset, transform.position.z);
     }
 }
